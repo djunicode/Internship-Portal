39ea894c42ac6e039ff7b511793e2bc8ec548f09
MeetMRP <meetpatelmrp1005@gmail.com>
2023-03-30 14:37:50 +0530

    commenced accounts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'rest_framework',
+
+    #apps
     'users',
+    'accounts',
 ]
 
 MIDDLEWARE = [
